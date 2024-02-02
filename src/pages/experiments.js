2ffed36c29b41edc59eeca2b6286c4885b71e62c
yashikaa-05 <147026363+yashikaa-05@users.noreply.github.com>
2024-02-02 21:05:39 +0530

    Update experiments.js

---
+++ src/pages/experiments.js
@@ -0,0 +1,12 @@
+import Layout from '@/Components/Global/Layout';
+import Navbar from '@/Components/Global/Navbar';
+import Contactt from '@/Components/LandingPage/Contactt';
+
+
+export default function Home() {
+    return (
+        <Layout>
+            <Contactt />
+        </Layout>
+    )
+}
